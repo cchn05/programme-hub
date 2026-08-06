08c6b0108edc644fd4041eb2470d6a19ed9aa67f
cchn05 <165762390+cchn05@users.noreply.github.com>
2026-08-06 10:07:13 +0800

    Connect Programme Hub to Supabase

---
+++ config.js
@@ -0,0 +1,7 @@
+export const CONFIG = Object.freeze({
+  supabaseUrl: 'https://irogyisudgtiacpwswnv.supabase.co',
+  supabasePublishableKey: 'sb_publishable_TwXf24DZxORSyWrX9fHzJA_HFb0I26-',
+  siteUrl: 'https://cchn05.github.io/programme-hub/',
+  photoBucket: 'project-photos',
+  publicAssetBucket: 'public-assets'
+});
